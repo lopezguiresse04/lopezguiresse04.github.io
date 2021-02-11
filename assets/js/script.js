5416563fc6a88f569723419e9c1f97180d3128ae
Alvaro Lopez Guiresse <lopez.guiresse.04@gmail.com>
2021-02-10 22:01:22 -0300

    se modifica scroll agregando efecto con js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,21 @@
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
+
+ 
+
+    $(function(){
+
+
+        $(window).scroll(function(){
+            var posicion = $(this).scrollTop();
+            //alert("me movieron");
+
+            if(posicion > 100){
+                $('.navbar').addClass('bg-scroll');
+            }  else {
+                $('.navbar').removeClass('bg-scroll');
+            }
+        });
+
+    });
